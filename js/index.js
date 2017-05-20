2e11eb1f86fa8023795b8cdc41d578b0c560dc96
Chris <chris.loewer@gmail.com>
2017-05-20 17:30:59 -0500

    Add scroll controller logic

---
+++ js/index.js
@@ -0,0 +1,37 @@
+
+function expandIntro() {
+  $('#intro-content').addClass('open');
+}
+
+function collapseIntro() {
+  $('#intro-content').removeClass('open');
+}
+
+window.addEventListener('load', function () {
+
+  function introController() {
+    var breakpoint = 400;
+    var currentPosition = getWindowScrollY();
+
+    if(currentPosition > breakpoint) {
+      expandIntro();
+    }
+    else {
+      collapseIntro();
+    }
+  }
+
+  window.addEventListener('scroll', introController, false);
+  introController();
+
+});
+
+
+function getWindowScrollY() {
+  if(window.scrollY !== undefined) {
+    return window.scrollY;
+  }
+  else {
+    return document.documentElement.scrollTop;
+  }
+}
