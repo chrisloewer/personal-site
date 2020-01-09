eb9c9f8cc4faaa7be7d4c4ddecef4c2274a6b89d
chris <chris.loewer@gmail.com>
2020-01-08 22:53:14 -0800

    Update packages and fix breaking changes to gulp.

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,10 +3,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('styles', function() {
-  gulp.src('styles/*.scss')
+function formatStyles() {
+  return gulp.src('styles/*.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest('styles'));
-});
+}
 
-gulp.task('default', ['styles']);
+exports.default = formatStyles;
